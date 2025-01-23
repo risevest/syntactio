136973e2980cb838dfcd28eaacaf34f6995bca7a
balogunofafrica <balogunakanbi.k@gmail.com>
2025-01-23 16:53:37 +0100

    add config

---
+++ README.md
@@ -0,0 +1,40 @@
+# @risemaxi/syntactio
+
+ESLint config for Rise client apps.
+
+## Installation
+
+```bash
+npm install -D @risemaxi/syntactio
+```
+
+OR
+
+```bash
+yarn add -D @risemaxi/syntactio
+```
+
+OR
+
+```bash
+bun install -D @risemaxi/syntactio
+```
+
+## Peer Dependencies
+
+- TypeScript ^5.0.0
+- ESLint ^9.0.0
+
+## Usage
+
+In `.eslintrc.js`:
+
+```javascript
+module.exports = {
+  extends: ["@risemaxi/syntactio"],
+};
+```
+
+## License
+
+MIT
---
+++ index.ts
@@ -0,0 +1,85 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@react-native-community",
+    "plugin:react/recommended",
+    "plugin:unicorn/recommended",
+    "prettier",
+  ],
+  globals: {
+    Atomics: "readonly",
+    NodeJS: true,
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "simple-import-sort",
+    "sort-keys-fix",
+    "unused-imports",
+    "typescript-sort-keys",
+    "unicorn",
+  ],
+  root: true,
+  rules: {
+    "comma-dangle": ["error", "never"],
+    "no-console": 2,
+    "no-extra-boolean-cast": 0,
+    "react/jsx-curly-brace-presence": [
+      1,
+      { children: "never", props: "never" },
+    ],
+    "react/jsx-sort-props": [
+      "error",
+      {
+        ignoreCase: true,
+      },
+    ],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/react-in-jsx-scope": "off",
+    semi: 0,
+    "simple-import-sort/exports": "warn",
+    "simple-import-sort/imports": "warn",
+    "sort-keys-fix/sort-keys-fix": "warn",
+    "typescript-sort-keys/interface": "warn",
+    "typescript-sort-keys/string-enum": "warn",
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          kebabCase: true,
+        },
+      },
+    ],
+    "unicorn/no-null": "off",
+    "unicorn/no-useless-undefined": "off",
+    "unicorn/prevent-abbreviations": [
+      "error",
+      {
+        replacements: {
+          args: false,
+          params: false,
+          props: false,
+          ref: false,
+        },
+      },
+    ],
+  },
+};
