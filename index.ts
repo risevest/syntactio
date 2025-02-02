2b1c5c18ed72f87762ebf763d8811b3e9423f50b
balogunofafrica <balogunakanbi.k@gmail.com>
2025-02-02 18:32:19 +0100

    add accessibility plugin

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "@react-native-community",
     "plugin:react/recommended",
+    "plugin:react-native-a11y/all",
     "prettier",
   ],
   globals: {
