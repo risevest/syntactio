419505c10ceeab0a6f277cce5f0715190a6a4b3a
balogunofafrica <balogunakanbi.k@gmail.com>
2025-04-16 06:44:54 +0100

    remove default overrides

--- index.ts
+++ index.ts
@@ -73,12 +73,4 @@ module.exports = {
     "react-compiler/react-compiler": "error",
     "import/no-default-export": "error",
   },
-  overrides: [
-    {
-      files: ["src/app/**/*", "app/**/*"],
-      rules: {
-        "import/no-default-export": "off",
-      },
-    },
-  ],
 };
